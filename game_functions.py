da06096d6fc55ec5555f2f72f9ca680e0e3fbe2c
iamosama <m.osama182@gmail.com>
2020-10-13 17:31:53 +0200

    Moving fleet of aliens

--- game_functions.py
+++ game_functions.py
@@ -115,3 +115,22 @@ def create_fleet(ai_settings, screen, ship, aliens):
     for row_number in range(number_rows):
         for alien_number in range(number_aliens_x):
             create_alien(ai_settings, screen, aliens, row_number, alien_number)
+
+
+def update_aliens(ai_settings, aliens):
+    """Check if fleet in at an edge and then update positions of all aliens"""
+    check_fleet_edges(ai_settings, aliens)
+    aliens.update()
+
+def check_fleet_edges(ai_settings, aliens):
+    """Respond appropriately if any aliens have reached an edge"""
+    for alien in aliens.sprites():
+        if alien.check_edges():
+            change_fleet_direction(ai_settings, aliens)
+            break
+
+def change_fleet_direction(ai_settings, aliens):
+    """Drop the entire fleet down and change direction"""
+    for alien in aliens.sprites():
+        alien.rect.y += ai_settings.fleet_drop_speed
+    ai_settings.fleet_direction *= -1
